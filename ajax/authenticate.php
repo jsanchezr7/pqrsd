b1db6f42cf22dcbc9b202da0428d30da54af7f9c
cbastin <cbax777@hotmail.com>
2025-10-20 12:40:09 -0500

    remove some display errors

--- ajax/authenticate.php
+++ ajax/authenticate.php
@@ -2,10 +2,9 @@
 // declare(strict_types=1);
 
 
-// Do not output PHP warnings/notices to the response body (they break API consumers)
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once "../extensiones/vendor/autoload.php";
 
@@ -14,12 +13,7 @@ use Firebase\JWT\JWT;
 
 if (isset($_SERVER['HTTP_REFERER'])){
 	if ($_SERVER['HTTP_REFERER']==('https://aplicaciones.mab.com.co/pqr/vistas/login.php')||('http://52.232.166.142/vistas/login.php')){
-		// Prefer secret from config/global.php if present
-		if (defined('JWT_SECRET_KEY') && JWT_SECRET_KEY) {
-			$secretKey = JWT_SECRET_KEY;
-		} else {
-			$secretKey  = ';[wR77BmCt"y~jXL7M:wu{cPV|-*IwtA2!d%_K..4t`i0;:&SjPtqFSeiW#`7?d';
-		}
+		$secretKey  = ';[wR77BmCt"y~jXL7M:wu{cPV|-*IwtA2!d%_K..4t`i0;:&SjPtqFSeiW#`7?d';
 		$issuedAt   = new DateTimeImmutable();
 		$expire     = $issuedAt->modify('+60 minutes')->getTimestamp();      // Add 60 seconds
 		$serverName = "http://localhost/";
@@ -33,14 +27,14 @@ if (isset($_SERVER['HTTP_REFERER'])){
 		    'userName' => $username                 // User name
 		];
 
-		// Encode the array to a JWT string and ensure only the token is output
-		// Use output buffering to capture any accidental notices and discard them
-		ob_start();
-		$jwt = JWT::encode($data, $secretKey, 'HS512');
-		// clean any buffered output (warnings/notices) so only the token is returned
-		ob_end_clean();
-		echo $jwt;
-		unset($secretKey);
+		// Encode the array to a JWT string.
+	    echo JWT::encode(
+	        $data,
+	        $secretKey,
+	        'HS512'
+	    );
+	    
+		unset($secretKey); 
 	}else{
 		echo('401 Unauthorized');
 	  exit;
